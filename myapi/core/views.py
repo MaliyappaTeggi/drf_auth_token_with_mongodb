811e2ca3a9c65b99f2cf3800446bc3d63560a66f
maliyappa <maliyappa04@gmail.com>
2020-04-27 09:17:44 +0530

    added jwt authentication permission

--- myapi/core/views.py
+++ myapi/core/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated  # <-- Here
+from rest_framework_simplejwt import authentication
 
 
 class HelloView(APIView):
     permission_classes = (IsAuthenticated,)
+    authentication_classes = (authentication.JWTAuthentication,)
 
     def get(self, request):
         content = {'message': 'Hello, Maliyappa'}
